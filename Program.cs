152d4cc46c5a975215946f35ec469b883ff5aab7
Bárbara Iáguita Oliveira Vieira <barbaraviera55@gmail.com>
2023-10-06 09:39:41 -0400

    segundo commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número: ");
+        double num = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite outro número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double resultado = num + num2;
+
+        Console.WriteLine($"O resultado da multiplicação é {resultado}");
+
+        Console.ReadKey();
+
+        Console.WriteLine("git");
+    }
+}
